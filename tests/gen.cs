3861d7d8b043572588a4455d36dd2379b6d751b0
ChrizziDerKek <69979195+ChrizziDerKek@users.noreply.github.com>
2026-06-20 02:21:34 +0200

    whoops

---
+++ tests/gen.cs
@@ -0,0 +1,25 @@
+﻿namespace testgen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 0;
+            string cmd = "svenstatictool clear-tile-rect tutorial01v01.lvl cal_empty.lvl 0 0 25 15\n";
+            string table = "";
+            for (int y = 0; y < 15; y++)
+            {
+                for (int x = 0; x < 25; x++)
+                {
+                    string id = c <= 9 ? "00" + c : c <= 99 ? "0" + c : "" + c;
+                    c++;
+                    cmd += string.Format("svenstatictool paint-tile-type cal_empty.lvl cal_RX{0}.lvl {1} {2} 1 1 1\n", id, x, y);
+                    cmd += string.Format("mudgetool patch-file XS.pak XS/levels/tutorial01v01.lvl cal_RX{0}.lvl XS_RX{0}.pak\n", id);
+                    table += string.Format("{0} (X={1};Y={2}): \n", id, x, y);
+                }
+            }
+            File.WriteAllText("run_single_tile_test.bat", cmd);
+            File.WriteAllText("table.txt", table);
+        }
+    }
+}
---
+++ tests/runner.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace test_runner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "current.txt";
+            if (!File.Exists(file))
+            {
+                File.Create(file).Close();
+                File.WriteAllText(file, "000");
+            }
+            string id = File.ReadAllText(file);
+            int next = string.IsNullOrEmpty(id.TrimStart('0')) ? 1 : (int.Parse(id.TrimStart('0')) + 1);
+            string target = "XS_RX" + id + ".pak";
+            id = next <= 9 ? "00" + next : next <= 99 ? "0" + next : "" + next;
+            File.WriteAllText(file, id);
+            File.Copy("tests/" + target, target);
+            Console.WriteLine("Testing " + target);
+            File.Move(target, "XS.pak", true);
+            Thread.Sleep(10);
+            Process.Start("cmd.exe", "/K start .\\Sven2.exe");
+            Environment.Exit(0);
+        }
+    }
+}
